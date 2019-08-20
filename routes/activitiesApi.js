a117e3ee7107bbc814fcb82826e918ef1534c79c
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-20 16:35:36 +0200

    update routes activityApi

--- routes/activitiesApi.js
+++ routes/activitiesApi.js
@@ -22,16 +22,22 @@ router.get('/oneActivity/:id', isLoggedIn(), async (req, res, next) => {
   console.log('PARAAAAAAAAMS ', req.params);
   try {
     const { id } = req.params;
-    const activitiesList = await Activity.findById(id);
-    const activity = activitiesList.filter((activity) => {
-      console.log('ID ----->', id);
+    const activity = await Activity.findById(id);
+    console.log(activity);
+    res.status(200).json(activity);
+  } catch (error) {
+    next(error);
+  }
+});
 
-      if (activity._id == id) {
-        return activity;
-      } else {
-        console.log('Not Found');
-      }
-    });
+router.put('/updateActivity/:id', isLoggedIn(), async (req, res, next) => {
+  console.log('BODY ', req.body);
+
+  try {
+    const { id } = req.params;
+    const activityUpdated = req.body;
+    const activity = await Activity.findByIdAndUpdate(id, activityUpdated, { new: true });
+    console.log(activity);
     res.status(200).json(activity);
   } catch (error) {
     next(error);
